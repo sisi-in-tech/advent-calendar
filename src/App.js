1d9ec5e9dfafbd93af66d11d9fbb4096446c7cd7
sisi-in-tech <si.sijun@outlook.com>
2023-12-25 14:37:59 -0800

    advent calendar 2023

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import doors from "./json/doors.json";
+import moment from 'moment-timezone';
 
 function App() {
+  const [isOpen, setIsOpen] = useState([]);
+  const [submitToStorage, setSubmitToStorage] = useState([]);
+  const checkIfAllowedToOpen = (inputdate) => {
+    const doordate = new Date(inputdate);
+    const today = new Date();
+
+    today >= doordate
+      ? openDoor(inputdate)
+      : alert(
+          " Not allowed to open yet, wait a few days"
+        );
+  };
+
+  const openDoor = (doordate) => {
+    setSubmitToStorage([...isOpen, doordate]);
+    localStorage.setItem("isOpen", submitToStorage);
+  };
+
+  const converDateToDay = (inputdate) => {
+    const date = moment.tz(inputdate, "America/Los_Angeles");
+    const day = date.date(); 
+    return day;
+  };
+  
+
+  const resetDoors = () => {
+    setSubmitToStorage([]);
+    localStorage.removeItem("isOpen");
+  };
+
+  useEffect(() => {
+    const local = localStorage.getItem("isOpen");
+    setIsOpen(local ? local : []);
+  }, [submitToStorage]);
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="content">
+        <div className="calendar">
+          {doors.map((door, i) => {
+            return (
+              <div key={i}>
+                {isOpen.includes(door.day) ? (
+                  <div className="dooropen">
+                     <a href={door.imageUrl} target="_blank" rel="noopener noreferrer">
+                    <p>{door.message}</p>
+                    </a>
+                  </div>
+                ) : (
+                  <div
+                    className="door"
+                    onClick={() => checkIfAllowedToOpen(door.day)}
+                  >
+                    <p>{converDateToDay(door.day)}</p>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </div>
+        <button className="resetbutton" onClick={resetDoors}>
+          Reset my calendar
+        </button>
+      </div>
     </div>
   );
 }
